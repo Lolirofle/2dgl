c1f0cefe31e4bf92cf9367b85cd01a80eb59fe42
Lolirofle <lolipopple@hotmail.com>
2014-11-04 04:35:11 +0100

    Rename gameloop structs

--- src/lolirofle/game/gameloop.rs
+++ src/lolirofle/game/gameloop.rs
@@ -1,11 +1,11 @@
 use lolirofle::game::Game;
 use lolirofle::gl::renderer::Renderer;
 
-pub trait Updatable<G: Game>{
+pub trait Update<G: Game>{
 	fn update(&mut self,game: &G,delta_time: f64);
 }
 
-pub trait Renderable{
+pub trait Render{
 	fn render(&self,renderer: &Renderer);
 }
 
